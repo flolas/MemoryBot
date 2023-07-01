43502c4a677bff98b90bfb7bc04a3488f61ae954
Felipe Lolas <flolas@alumnos.uai.cl>
2023-07-01 17:36:27 -0400

    sdffdsdf

--- memorybot.py
+++ memorybot.py
@@ -82,7 +82,7 @@ st.title("Radiografia Financiera")
 st.subheader("Conoce cómo están tus finanzas!")
 
 
-agree = st.checkbox('Doy mi consentimiento para el tratamiento de mis datos utilizando esta página y proveedor Fintoc para que se me entregue una asesoría y diagnotisco de mis finanzas.')
+agree = st.checkbox('Doy mi consentimiento para el tratamiento de mis datos utilizando esta página y proveedor Fintoc con la finalidad de que se me entregue una asesoría y diagnotisco de mis finanzas.')
 st.caption('Los datos serán eliminados una vez que cierres el explorador, si quieres volver a utilizar la herramienta debes ingresar nuevamente tus datos bancarios.')
 
 open_modal = st.button("Conectar mis cuentas bancarias 🔌 🏦", disabled = not agree)
