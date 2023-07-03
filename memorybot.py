0b1559b4ce42ce8136f9cf4d47088fbf9727a266
Felipe Lolas <flolas@alumnos.uai.cl>
2023-07-03 01:46:09 -0400

    sdffdfda

--- memorybot.py
+++ memorybot.py
@@ -269,7 +269,7 @@ with st.container():
             st.plotly_chart(plot, theme="streamlit", use_container_width=True)
     if prompt:
         pandas_ai = initialize_langchain_agent()
-        output = pandas_ai(list(st.session_state["fintoc_data"].values()), prompt)
+        output = pandas_ai.run(list(st.session_state["fintoc_data"].values()), prompt)
         st.session_state.past.append(prompt)  
         st.session_state.generated.append(output) 
         for idx, user_message in enumerate(st.session_state.past):
